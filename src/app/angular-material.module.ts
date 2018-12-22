68a7a620586888a7bbf0468dd37ff5834103a45e
VarArenz028 <arenz.villarino@value-exch.com>
2018-12-22 20:49:45 +0800

    Added sass files and setup material module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [MatFormFieldModule, MatInputModule],
+  declarations: []
+})
+export class AngularMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
